1a35dff13bc51535e36cf48720bfcb8d59ece941
Иванов Константин <konstantin.ivanov39@gmail.com>
2017-10-05 22:44:36 +0300

    дз 3.7 после доработки

--- HomeWork_3.7/homework_3.7.py
+++ HomeWork_3.7/homework_3.7.py
@@ -21,37 +21,55 @@ class YandexMetrika:
         response = requests.get(self.management_url, headers=headers)
         return response.json()['counters']
 
-    def get_counter_visits(self, counter_id, ):
+
+class Counter:
+
+    stat_url = 'https://api-metrika.yandex.ru/stat/v1/data'
+
+    def __init__(self, token, counter_id):
+        self.token = token
+        self.counter_id = counter_id
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_counter_visits(self, ):
         headers = self.get_headers()
         params = {
-        'id': counter_id,
+        'id': self.counter_id,
         'metrics': 'ym:s:visits'
         }
         response = requests.get(self.stat_url, params, headers=headers)
         return response.json()['totals']
 
-    def get_counter_views(self, counter_id, ):
+    def get_counter_views(self, ):
         headers = self.get_headers()
         params = {
-        'id': counter_id,
+        'id': self.counter_id,
         'metrics': 'ym:s:pageviews'
         }
         response = requests.get(self.stat_url, params, headers=headers)
         return response.json()['totals']
 
-    def get_counter_users(self, counter_id, ):
+    def get_counter_users(self, ):
         headers = self.get_headers()
         params = {
-        'id': counter_id,
+        'id': self.counter_id,
         'metrics': 'ym:s:users'
         }
         response = requests.get(self.stat_url, params, headers=headers)
         return response.json()['totals']
 
+
 metrika = YandexMetrika(TOKEN)
 counters = metrika.get_counters()
-for counter in counters:
-    print('По счетчику:', counter['name'], 'статистика составляет:')
-    print('Суммарное количество визитов:', metrika.get_counter_visits(counter['id']))
-    print('Число просмотров страниц на сайте:', metrika.get_counter_views(counter['id']))
-    print('Количество уникальных посетителей:', metrika.get_counter_users(counter['id']))
+counter_id = counters[0]['id']
+
+counter = Counter(TOKEN, counter_id)
+
+print('Суммарное количество визитов:', counter.get_counter_visits())
+print('Число просмотров страниц на сайте:', counter.get_counter_views())
+print('Количество уникальных посетителей:', counter.get_counter_users())
